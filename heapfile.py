5b548f7d1f7f4ea75feb4395fbd3667653a2b47b
Spookstycal <uvis.virsnitis@gmail.com>
2025-06-04 20:31:56 +0300

    Fixed minheap property

--- heapfile.py
+++ heapfile.py
@@ -75,47 +75,52 @@ class MinHeap:
         return 2 * index + 1
     
     def _right_child(self, index):
-        return 2*index + 2
+        return 2 * index + 2
     
-    def _parent(self,index):
+    def _parent(self, index):
         return (index - 1) // 2
     
     def _swap(self, index1, index2):
         self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
 
     def insert(self, value):
+            
         self.heap.append(value)
         current = len(self.heap) - 1
 
-        while current > 0 and (self.parameter(self.heap[current]) < self.parameter(self.heap[self._parent(current)])):
+        while (current > 0 and 
+               self.parameter(self.heap[current]) < self.parameter(self.heap[self._parent(current)])):
             self._swap(current, self._parent(current))
             current = self._parent(current)
 
     def remove(self):
-        if len(self.heap) == 0:
+        if not self.heap:
             return None
         
         if len(self.heap) == 1:
             return self.heap.pop()
         
-        max_value = self.heap[0]
+        min_value = self.heap[0]
         self.heap[0] = self.heap.pop()
         self._sink_down(0)
-
-        return max_value
+        return min_value
     
     def _sink_down(self, index):
-        max_index = index
+        min_index = index
         while True:
-            left_index = self._left_child(index)
-            right_index = self._right_child(index)
-
-            if (left_index < len(self.heap)) and (self.parameter(self.heap[left_index]) < self.parameter(self.heap[max_index])):
-                max_index = left_index
-
-            if (right_index < len(self.heap)) and (self.parameter(self.heap[right_index]) < self.parameter(self.heap[max_index])):
-                max_index = right_index
-
-            if max_index != index:
-                self._swap(index, max_index)
-                index = max_index        
+            left = self._left_child(index)
+            right = self._right_child(index)
+            
+            if (left < len(self.heap) and 
+                self.parameter(self.heap[left]) < self.parameter(self.heap[min_index])):
+                min_index = left
+                
+            if (right < len(self.heap) and 
+                self.parameter(self.heap[right]) < self.parameter(self.heap[min_index])):
+                min_index = right
+                
+            if min_index == index:
+                break
+                
+            self._swap(index, min_index)
+            index = min_index
